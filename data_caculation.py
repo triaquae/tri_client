7b47bac208bcd092dca5bda78b0e7242e833780e
Alex <lijie3721@126.com>
2014-04-07 20:27:38 +0800

    data handle feature

---
+++ data_caculation.py
@@ -0,0 +1,18 @@
+
+#a=[[1396766780.617834, "0.00", "0.67", "99.33", "0.00"], [1396766838.235313, "0.00", "0.84", "96.79", "2.36"], [1396766943.867216, "0.00", "0.67", "96.96", "2.36"], [1396868395.640945, "0.00", "0.51", "97.31", "2.19"]]
+
+
+def convert_to_float(status):
+        status_list = [float(x ) for x in status[1:]]
+	return status_list
+
+def get_average(source_data):
+	data_list=map(convert_to_float, source_data)
+	total_value = [0 for i in range(len(data_list[0])) ] #initialize total value list
+	for i in data_list:
+	  for index,n in enumerate(i):
+		total_value[index] += n
+
+
+	avg = [round(x/len(data_list),2)  for x in  total_value ] 
+	return avg
--- status_data_optimzation.py
+++ status_data_optimzation.py
@@ -25,7 +25,7 @@ for h,p_index_list in  get_monitor_host_list().items():
 			last_time_in_db =  STATUS_DATA[service]['Actual'][-1][0] 
 			if last_check_stamp is not None:
 				if last_check_stamp - last_time_in_db < 10: #minimum 10 s interval
-					print '\033[32;1mduplicate point......\033[0m'
+					print '\033[32;1mduplicate point......\033[0m', service, h.hostname
 					continue
 				else:
 					STATUS_DATA[service]['Actual'].append( [last_check_stamp] )
@@ -33,15 +33,25 @@ for h,p_index_list in  get_monitor_host_list().items():
 		  else: # add serivce name into dic if it's not in there
 			STATUS_DATA[service] = {}
 			STATUS_DATA[service]['Actual'] = [[last_check_stamp ] ]
+			STATUS_DATA[service]['last_check_index_dic'] = {} # save the end data point of each data set 
 			for dataset_name,interval in data_storage_interval.items(): 
-				STATUS_DATA[service][dataset_name] = []
-		  #print service, obj.graph_index
-		  #print '-------->',latest_status[h.hostname].get(service)
+				STATUS_DATA[service][dataset_name] = [[last_check_stamp] ]
+				STATUS_DATA[service]['last_check_index_dic'][dataset_name] = 0
 		  for index_name in  obj.graph_index['index']: #graph list 
 		    try:
-		      #print index_name
+		      print index_name,'++++++++'
 		      index_value = latest_status[h.hostname][service][index_name] 
 		      STATUS_DATA[service]['Actual'][-1].append(index_value)
+		      current_time_stamp = time.time()
+		      for dataset_name,interval in data_storage_interval.items():
+			last_point_time = STATUS_DATA[service][dataset_name][-1][0]
+			if current_time_stamp - last_point_time < interval[0]:  #interval[0] is the step,interval[1] is max point limit
+				print "\033[43;1m not hitting the interval yet\033[0m", dataset_name
+			else:
+				period_start_index = STATUS_DATA[service]['last_check_index_dic'][dataset_name] + 1 
+				if len( STATUS_DATA[service]['Actual'][period_start_index:] ) > 0: #has new data since last data collection	
+					print '-------->>>',STATUS_DATA[service]['Actual'][period_start_index:]
+				print '\033[42;1m ---->hitting hte interval ...\033[0m', dataset_name
 		    except KeyError:pass
 	redis.r['STATUS_DATA::%s' % h.hostname] = json.dumps(STATUS_DATA)
 	for n,v in STATUS_DATA.items():
@@ -50,11 +60,9 @@ for h,p_index_list in  get_monitor_host_list().items():
 		for i,n in v.items():
 			if i == 'Actual':
 			  for d in n:
-				print d, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(d[0] ))
+				print 'Actual',d, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(d[0] ))
 			else:
 				print i,n
-		    #except ValueError:pass
-#print latest_status
 		      
 			
 			

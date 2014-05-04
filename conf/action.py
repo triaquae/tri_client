0aa2d1a464aef69532ca6b2174c2b9050e669f30
Alex <lijie3721@126.com>
2014-05-04 22:00:04 +0800

    alert feature

--- conf/action.py
+++ conf/action.py
@@ -1,20 +1,50 @@
 #!/usr/bin/env python
 severity = {
-	'Unimportant' : [ record,1],
-	'Information': [ record, 1],
-	'Warning' : [ email,1 ,5 escalate],
-	'Critical':[email, sms, 2,escalate],
-	'Urgent': [email,sms, 1,escalate],
-	'Disaster' : []
+	'Unimportant' : {
+		're_try': 5,
+		'alert': ['record'],
+		'recover_notice': False,
+		'action': None , #['scriptName'],
+		'escalate': [20, 'Warning'] #retry 20 times,then escalate to Critical level
+		},
+	'Warning':{
+		're_try': 3,
+                'alert': ['email'],
+                'recover_notice': False,
+                'action': None , 
+                'escalate': [20, 'Critical'] 
+		},
+	'Critical':{
+                're_try': 2,
+                'alert': ['email','sms'],
+                'recover_notice': True,
+                'action': None,
+                'escalate': [15, 'Urgent']
+		},
+	'Urgent': {
+                're_try': 1,
+                'alert': ['email','sms','wei_chat'],
+                'recover_notice': True,
+                'action': None ,
+                'escalate': [10, 'Disaster']
+		},
+	'Disaster' : {
+		're_try': 1,
+                'alert': ['email','sms','wei_chat'],
+                'recover_notice': True,
+                'action': None ,
+                'escalate': [10, 'Disaster']
+		}
 }
 
-action_obj {
-host 
-Severity_type 
-group
-service
-template
-}
+opreator = ['>','<', '=', 'contains', 'starts_with','in','btween']
+obj ={
+	'host' : ['=', 'contains', 'starts_with','in'], 
+	'level ' : ['='],
+	'group' :['=', 'contains', 'starts_with','in'],
+	'service' : ['=']
+	'template' : ['='],
+	}
 
 notifier = {
 	alex
@@ -26,7 +56,7 @@ opreator = [> < = contains starts in ]
 if host = 'localhost' and service  = 'cpu':
 	action = 'Urgent' 
 
-
+if host 
 
 
 
